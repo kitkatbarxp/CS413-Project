7473d2ada001b9466e9156a1c6d351ddecb94d8b
Kit Tse <kitsumtse@gmail.com>
2016-11-27 23:06:37 -0500

    Added Sub as an op

--- 0.1.hs
+++ 0.1.hs
@@ -4,27 +4,33 @@ data Op a where
   I      :: Int -> Op Int 
   IL     :: [Int] -> Op [Int]
   AddI   :: Op Int -> Op Int -> Op Int
+  SubI   :: Op Int -> Op Int -> Op Int
 
   D      :: Double -> Op Double
   DL     :: [Double] -> Op [Double]
   AddD   :: Op Double -> Op Double -> Op Double
+  SubD   :: Op Double -> Op Double -> Op Double
 
 instance (Show a) => Show (Op a) where
   show (I n) = show n
   show (IL l) = show l
   show (AddI o1 o2) = "Add " ++ show o1 ++ show o2
+  show (SubI o1 o2) = "Sub " ++ show o1 ++ show o2
 
   show (D n) = show n
   show (DL l) = show l
   show (AddD o1 o2) = "Add " ++ show o1 ++ show o2
+  show (SubD o1 o2) = "Sub " ++ show o1 ++ show o2
 
 eval :: Op a -> a
 eval (IL l) = l
 eval (I n) = n
 eval (AddI o1 o2) = eval o1 + eval o2
+eval (SubI o1 o2) = eval o1 - eval o2
 eval (DL l) = l
 eval (D n) = n
 eval (AddD o1 o2) = eval o1 + eval o2
+eval (SubD o1 o2) = eval o1 - eval o2
 
 data Expr = MapI (Op [Int]) (Op Int -> Op Int) (Op [Int]) 
           | MapD (Op [Double]) (Op Double -> Op Double) (Op [Double])
@@ -39,6 +45,7 @@ run a@(MapD _ _ (DL [])) = a
 run (MapI (cur) (exp) (args)) = MapI (IL ((eval cur) ++ [eval(exp(I (head (eval args))))])) (exp) (IL (drop 1 (eval args)))
 run (MapD (cur) (exp) (args)) = MapD (DL ((eval cur) ++ [eval(exp(D (head (eval args))))])) (exp) (DL (drop 1 (eval args)))
 
-m = MapI (IL []) (AddI (I 10)) (IL [1,2,3,4])
-m' = MapD (DL []) (AddD (D 10)) (DL [1,2,3,4])
-
+mIAdd = MapI (IL []) (AddI (I 10)) (IL [1,2,3,4])
+mISub = MapI (IL []) (SubI (I 1)) (IL [1,2,3,4])
+mDAdd = MapD (DL []) (AddD (D 10)) (DL [1,2,3,4])
+mDSub = MapI (IL []) (SubI (I 1)) (IL [1,2,3,4])
