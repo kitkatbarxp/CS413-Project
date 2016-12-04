db00898f85f873761f6b725d75f26bb29d52ed27
Kit Tse <kitsumtse@gmail.com>
2016-12-04 17:29:05 -0500

    added lambda data type and its printing; also added more printing steps

--- 0.1.hs
+++ 0.1.hs
@@ -10,34 +10,55 @@ instance (Show a) => Show (Op a) where
     show (IL l) = show l
     show (Add o1 o2) = show o1 ++ " + " ++ show o2
 
-
 eval :: Op a -> a
 eval (IL l) = l
 eval (I n) = n
 eval (Add o1 o2) = eval o1 + eval o2
 
-data Expr = Map (Op [Int]) (Op Int -> Op Int) (Op [Int]) 
+
+-- lambda
+data LExpr where
+    LAdd :: Op Int -> LExpr
+
+instance Show LExpr where
+    show (LAdd op) = "(+ " ++ show op ++ ")"
+
+lAddToAdd :: LExpr -> (Op Int -> Op Int)
+lAddToAdd (LAdd n) = Add n
+
+
+
+-- map
+data Expr = Map (Op [Int]) LExpr (Op [Int])
 
 instance Show Expr where
-    show (Map a b c) = "Map" ++ show a ++ show c
+    show (Map a b c) = "map " ++ show a ++ " " ++ show b ++ " " ++ show c
 
 run' :: Expr -> Expr
 run' a@(Map _ _ (IL [])) = a
-run' m@(Map (cur) (exp) (args)) = Map (IL (baseCase ++ [ eval . exp $ nextVal ])) exp (IL remainder)
+run' m@(Map cur lExp args) = Map (IL (baseCase ++ [ eval . exp $ nextVal ])) lExp (IL remainder)
     where baseCase = eval cur
           nextVal  = I (head (eval args))
           remainder = drop 1 (eval args)
+          exp = lAddToAdd lExp
+
 
 run :: Expr -> IO ()
 run (Map cur _ (IL [])) = putStrLn $ show cur
-run m@(Map (cur) (exp) (args)) = do
+run m@(Map cur lExp args) = do
     let bc = eval cur
     let nextVal = I (head (eval args))
     let remainder = drop 1 (eval args)
+    let op = lAddToAdd lExp
+    let exp = op nextVal
+    let evalExp = eval exp
     let state = run' m
-    putStrLn $ show bc ++ " : " ++ show (exp nextVal) ++ " : map " ++ show remainder 
-    putStrLn $ show (bc ++ [eval . exp $ nextVal]) ++ " : map " ++ show remainder
+
+    putStrLn $ show m
+    putStrLn $ "   1. " ++ show bc ++ " : " ++ show exp ++ " : map " ++ show lExp ++ " " ++ show remainder 
+    putStrLn $ "   2. " ++ show bc ++ " : " ++ show evalExp ++ " : map " ++ show lExp ++ " " ++ show remainder
+    putStrLn $ "   3. " ++ show (bc ++ [evalExp]) ++ " : map " ++ show lExp ++ " "  ++ show remainder ++ "\n"
     run state
 
-m = Map (IL []) (Add (I 10)) (IL [1,2,3,4])
+m = Map (IL []) (LAdd (I 10)) (IL [1,2,3,4])
 
